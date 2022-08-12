1d4bf78455a450f624797698260ba6f09be938a8
Priscilla Colon <plynn9036@gmail.com>
2022-08-12 08:24:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  BrowserRouter, Routes, Route
-} from '.react-router-dom';
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
