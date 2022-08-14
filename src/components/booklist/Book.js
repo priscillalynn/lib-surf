9b59115cd9699edb40fe81a721006e6c1555e3c5
Priscilla Colon <plynn9036@gmail.com>
2022-08-14 15:28:38 -0400

    updated

---
+++ src/components/booklist/Book.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../booklist/booklist.css';
+
+const Book = (book) => {
+  return (
+    <div className='book-item flex flex-bolumn flex-sb'>
+        <div className='book-item-img'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Book
