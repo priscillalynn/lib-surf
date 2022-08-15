7957b1c1e2a24ae527bf9b07ff6cf1c70ba22ffc
Priscilla Colon <plynn9036@gmail.com>
2022-08-14 20:25:09 -0400

    fixed

--- src/components/book details/BookDetails.js
+++ src/components/book details/BookDetails.js
@@ -1,42 +1,43 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import Loader from '../loader/Loader';
+import Loading from "../loader/Loader";
 import coverImg from "../../assets/notavailable.jpg";
-import './bookdetails.css';
-import {FaArrowLeft} from 'react-icons/fa';
+import "./bookdetails.css";
+import {FaArrowLeft} from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 
-const URL = "https://openlibrary.org/works/OL45883W.json";
+const URL = "https://openlibrary.org/works/";
 
 const BookDetails = () => {
   const {id} = useParams();
   const [loading, setLoading] = useState(false);
-  const [book, setBooks] = useState(null);
+  const [book, setBook] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     setLoading(true);
-    async function getBookDetails() {
-      try {
+    async function getBookDetails(){
+      try{
         const response = await fetch(`${URL}${id}.json`);
         const data = await response.json();
+        console.log(data);
 
-        if(data) {
+        if(data){
           const {description, title, covers, subject_places, subject_times, subjects} = data;
           const newBook = {
             description: description ? description.value : "No description found",
             title: title,
-            cover_img: covers ? `https://covers.openlibrary.org/b/id${covers[0]}-L.jpg` : coverImg,
+            cover_img: covers ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg` : coverImg,
             subject_places: subject_places ? subject_places.join(", ") : "No subject places found",
-            subject_times: subject_times ? subject_times.join(", ") : "No subject times found",
+            subject_times : subject_times ? subject_times.join(", ") : "No subject times found",
             subjects: subjects ? subjects.join(", ") : "No subjects found"
           };
-          setBooks(newBook);
+          setBook(newBook);
         } else {
-          setBooks(null);
+          setBook(null);
         }
         setLoading(false);
-      } catch(error) {
+      } catch(error){
         console.log(error);
         setLoading(false);
       }
@@ -44,29 +45,44 @@ const BookDetails = () => {
     getBookDetails();
   }, [id]);
 
-  if(loading) return <Loader />;
+  if(loading) return <Loading />;
 
   return (
     <section className='book-details'>
       <div className='container'>
-        <button type='button' className='flex flex-c back-btn'onClick={() => navigate("/book")}>
-          <FaArrowLeft size={22} />
+        <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/book")}>
+          <FaArrowLeft size = {22} />
           <span className='fs-18 fw-6'>Go Back</span>
         </button>
 
         <div className='book-details-content grid'>
           <div className='book-details-img'>
-            <img src= {book?.cover_img} alt = 'cover' />
+            <img src = {book?.cover_img} alt = "cover img" />
           </div>
           <div className='book-details-info'>
             <div className='book-details-item title'>
-              
+              <span className='fw-6 fs-24'>{book?.title}</span>
+            </div>
+            <div className='book-details-item description'>
+              <span>{book?.description}</span>
+            </div>
+            <div className='book-details-item'>
+              <span className='fw-6'>Subject Places: </span>
+              <span className='text-italic'>{book?.subject_places}</span>
+            </div>
+            <div className='book-details-item'>
+              <span className='fw-6'>Subject Times: </span>
+              <span className='text-italic'>{book?.subject_times}</span>
+            </div>
+            <div className='book-details-item'>
+              <span className='fw-6'>Subjects: </span>
+              <span>{book?.subjects}</span>
             </div>
           </div>
         </div>
       </div>
     </section>
-    )
+  )
 }
 
 export default BookDetails
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
+import aboutImg from '../../assets/bookhold.jpg';
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
+
+        <div className='about-content grid'>
+          <div className='about-img'>
+            <img src= {aboutImg} alt=" "/>
+          </div>
+
+          <div className='about-text'>
+            <h2 className='about-title fs-26 ls-1'>About LibSurf</h2>
+            <p className='fs-17'>Lorem ipsum</p>
+            <p className='fs-17'>Lorem ipsum</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
