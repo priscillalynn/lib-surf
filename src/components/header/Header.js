72aa458b50f18daf483800c1137df69050d0746c
Priscilla Colon <plynn9036@gmail.com>
2022-08-21 16:56:50 -0400

    changed photo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,6 @@ const Header = () => {
         <Navbar />
         <div className='header-content flex flex-c text-center text-white'>
           <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
-          <p className='header-text fs-18 fw-3'>Genres: </p>
           <SearchForm />
         </div>
       </header>
